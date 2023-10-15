b6feddc16c9cc7e3273ab1582607709f45c87c43
David Galvez <david.dmg17@gmail.com>
2023-10-14 23:29:08 -0700

    Adding main page with background img

---
+++ personal-portfolio/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='Main'>
+        <img className='' src="./images/IMG_0448" alt="" />
+    </div>
+  )
+}
+
+export default Main
