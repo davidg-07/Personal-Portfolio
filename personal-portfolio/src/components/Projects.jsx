d9631acdab1593800a8f789eef0c4f6826f27ebd
David Galvez <david.dmg17@gmail.com>
2023-10-24 20:16:13 -0700

    added procjects

--- personal-portfolio/src/components/Projects.jsx
+++ personal-portfolio/src/components/Projects.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import ProjectItem from './ProjectItem';
+import weatherImg from '../images/weather.png'
+import scheduleImg from '../images/schedule.png'
+import notesImg from '../images/notes.png'
+import textImg from '../images/text.png'
+
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div id='Projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
+        <p className='text-center py-4'>Hello</p>
+        <div className='grid sm:grid-cols-2 gap-12'>
+            <ProjectItem img={weatherImg} title='Weather App'/>
+            <ProjectItem img={scheduleImg} title='Day Scheduler App'/>
+            <ProjectItem img={notesImg} title='Note Taker App'/>
+            <ProjectItem img={textImg} title='Text Editor App'/>
+        </div>
+    </div>
   )
 }
 
